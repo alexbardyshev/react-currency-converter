e226ceeda996c644b616afa2e4e8cc1c72fc1c6d
Oleksìi <bardyshev@gmail.com>
2022-09-11 20:33:46 +0300

    currency exchange form is ready

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import {CurrencyInput} from "./CurrencyInput";
+import {useState, useEffect} from "react";
+import axios from "axios";
+import {Grid} from "@material-ui/core";
 
 function App() {
     const [amount1, setAmount1] = useState(1);
@@ -8,16 +11,77 @@ function App() {
     const [currency1, setCurrency1] = useState('UAH');
     const [currency2, setCurrency2] = useState('USD');
 
+    const [rates, setRates] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://data.fixer.io/api/latest?access_key=106ab470d06b4c14d00c10f864ef62b6')
+            .then(response => {
+                setRates(response.data.rates);
+            })
+    }, []);
+
+    useEffect(() => {
+        if (!rates) {
+            function init() {
+                handleAmount1Change(1);
+            }
+            init();
+        }
+    }, [rates]);
+
+
+
+    function handleAmount1Change(amount1) {
+        setAmount2(roundUp(amount1 * rates[currency2] / rates[currency1]));
+        setAmount1(amount1);
+
+    }
+
+    function handleCurrency1Change(currency1) {
+        setAmount2(roundUp(amount1 * rates[currency2] / rates[currency1]));
+        setCurrency1(currency1);
+    }
+
+    function handleAmount2Change(amount2) {
+        setAmount1(roundUp(amount2 * rates[currency1] / rates[currency2]));
+        setAmount2(amount2);
+    }
+
+    function handleCurrency2Change(currency2) {
+        setAmount1(roundUp(amount2 * rates[currency1] / rates[currency2]));
+        setCurrency2(currency2);
+    }
+
+    function roundUp(number) {
+        return number.toFixed(2);
+    }
+
     return (
     <div className="App">
-      <CurrencyInput
-          amount={amount1}
-          currency={currency1}
-      />
-      <CurrencyInput
-          amount={amount2}
-          currency={currency2}
-      />
+        <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            style={{ minHeight: '100vh' }}
+        >
+            <h1>React Currency Exchanger</h1>
+              <CurrencyInput
+                  amount={amount1}
+                  currency={currency1}
+                  currencies={Object.keys(rates)}
+                  onAmountChange={handleAmount1Change}
+                  onCurrencyChange={handleCurrency1Change}
+              />
+              <CurrencyInput
+                  amount={amount2}
+                  currency={currency2}
+                  currencies={Object.keys(rates)}
+                  onAmountChange={handleAmount2Change}
+                  onCurrencyChange={handleCurrency2Change}
+              />
+        </Grid>
     </div>
   );
 }
--- src/CurrencyInput.js
+++ src/CurrencyInput.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import {array, number, string} from "prop-types";
+import PropTypes from "prop-types";
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Input from '@mui/material/Select';
 
 export const CurrencyInput = (props) => {
     return (
         <div className="currency-input">
-            <input type="text" value={props.amount} />
-            <select value={props.currency}>
-                {props.currencies.map((currency => (
-                    <option value={currency}>{currency}</option>
-                )))}
-            </select>
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 250 }}>
+                <input
+                    value={props.amount}
+                    onChange={(event) => props.onAmountChange(event.target.value)}
+                />
+                <Select
+                    labelId="demo-simple-select-standard-label"
+                    id="demo-simple-select-standard"
+                    value={props.currency}
+                    onChange={(event) => props.onCurrencyChange(event.target.value)}
+                >
+                    {props.currencies.map((currency => (
+                        <MenuItem value={currency}>{currency}</MenuItem>
+                    )))}
+                </Select>
+            </FormControl>
         </div>
     )
 }
 
 CurrencyInput.propTypes = {
-    amount: number,
+    amount: number | string,
     currency: string,
     currencies: array,
+    onAmountChange: PropTypes.func,
+    onCurrencyChange: PropTypes.func,
 }
